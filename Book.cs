36a419b5248e8aa0b0b81a14d8ce84dc8431e174
Joby John <joby432john@gmail.com>
2022-08-14 19:03:53 +0530

    Add project files.

---
+++ Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Book
+{
+	public string title;
+	public string author;
+	public int pages;
+	public Book()
+	{
+		//constructor with no parameter
+		Console.Write("constructor with No parameter");
+	}
+	public Book(string aTitle, string aAuthor, int aPages)
+    {
+		//consturctor with parameters
+        title = aTitle;
+        author = aAuthor;
+        pages = aPages;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Book book1 = new Book("harry potter", "JK rowling", 600);
+Console.WriteLine(book1.title);
+Book book2 = new Book();
